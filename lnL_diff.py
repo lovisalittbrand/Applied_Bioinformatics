5b848c84ef9927ca2ba0531369f6fd3cd225b50e
FerallOut <49016186+FerallOut@users.noreply.github.com>
2020-11-11 18:18:17 +0100

    Update lnL_diff.py

--- lnL_diff.py
+++ lnL_diff.py
@@ -3,20 +3,13 @@ import plotly.graph_objects as go
 
 with open('turtle_site_lk.sitelh') as f:
     df = pd.read_table(f, delim_whitespace=True, header=None, skiprows=1, index_col=0, lineterminator='\n')
-'''
-print(f'Shape is: {df.shape}')
-print(f'The types are: {df.dtypes}') 
-print(f'Information about the df: {df.info}')
-'''
+
+# calculate the T1-T2 difference between the log likelihoods for each position
 df.loc['Diff']=df.loc['Tree1'].values - df.loc['Tree2'].values
-'''
-print(f'Shape is: {df.shape}')
-print(f'The types are: {df.dtypes}') 
-print(f'Information about the df: {df.info}')
-df.loc['Diff'].describe()
-'''
+
 # no binning - display each number per position
-df2=df.loc['Diff'][0:50]        # I took out 50 positions for displaying; did not work for me with all 187026 positions
+####################################################
+df2=df.loc['Diff'][0:1000]        # I took out 1000 positions for displaying
 
 # set color 'green' if T1-T2 >= 0, else if T1-T2 < 0 set color 'red'
 clrs  = ['green' if i >= 0 else 'red' for i in df2]     
@@ -25,4 +18,14 @@ fig = go.Figure(data=[go.Bar(
             x=df2.index, y=df2,
             marker=dict(color=clrs)
         )])
+fig.show()
+#####################################################
+# for all 187026 positions - you can tell it worked if you change the background from gray to white:
+clrs2  = ['green' if i >= 0 else 'red' for i in df.iloc[2]]
+
+fig = go.Figure(data=[go.Bar(
+            x=df.iloc[2].index, y=df.iloc[2],
+            marker=dict(color=clrs2)
+        )])
+fig.update_layout(plot_bgcolor='rgba(0,0,0,0)')
 fig.show()
